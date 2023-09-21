12c318345a97a10c38690e36c29793a15586cffd
Sharmi Khan <145687416+Sharmi-Khan@users.noreply.github.com>
2023-09-21 21:49:09 +0530

    Add files via upload

---
+++ SecondActivity.java
@@ -0,0 +1,74 @@
+package com.suryadipta.socialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.squareup.picasso.Picasso;
+import com.suryadipta.socialapp.MainActivity;
+import com.suryadipta.socialapp.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class SecondActivity extends AppCompatActivity {
+    ImageView imageView;
+    TextView name;
+    Button logOutbtn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.suryadipta.socialapp.R.layout.activity_second);
+
+        imageView = findViewById(R.id.imageview);
+        name= findViewById(R.id.name);
+        logOutbtn = findViewById (R.id.logout);
+
+        AccessToken accessToken = AccessToken.getCurrentAccessToken ();
+
+        GraphRequest request = GraphRequest.newMeRequest(
+                accessToken,
+                new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(
+                            JSONObject object,
+                            GraphResponse response) {
+                        try {
+                            String fullName = object.getString("name");
+                            String url = object.getJSONObject("picture").getJSONObject("data").getString("url");
+                            name.setText(fullName);
+                            Picasso.get().load(url).into(imageView);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        // Application code
+                    }
+                });
+
+        Bundle parameters = new Bundle();
+        parameters.putString("fields", "id,name,link,picture");
+        request.setParameters(parameters);
+        request.executeAsync();
+
+        logOutbtn.setOnClickListener(new View.OnClickListener (){
+            @Override
+            public void onClick(View v){
+                LoginManager.getInstance().logOut();
+                startActivity(new Intent(SecondActivity.this, MainActivity.class));
+                finish();
+            }
+
+        });
+    }
+}
